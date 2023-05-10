2f6a40336cad121e18997977530937b195715743
caicia <42534127+caicia@users.noreply.github.com>
2023-05-10 15:34:12 +0800

    Update main.py

--- main.py
+++ main.py
@@ -7,15 +7,15 @@ if __name__ == "__main__":
     d.set_fastinput_ime(False)
     d(resourceId="cn.damai:id/homepage_advert_pb").click()
     d(resourceId="cn.damai:id/channel_search_text").click()
-    d.sleep(0.5)
-    d(resourceId="cn.damai:id/header_search_v2_input").send_keys('五月天2023诺亚方舟10周年')
+    d.sleep(1)
+    d(resourceId="cn.damai:id/header_search_v2_input").send_keys('梁静茹世界巡回演唱会苏州站')
     d.click(0.913, 0.895)
 
     d.xpath('//*[@resource-id="cn.damai:id/one_arch_recyclerView"]/android.widget.FrameLayout[1]').click()
-    d.sleep(0.1)
+    d.sleep(1)
 
     d(resourceId="cn.damai:id/damai_theme_dialog_cancel_btn").click_exists()
-    d.sleep(0.1)
+    d.sleep(1)
 
     d.click(0.608, 0.95)
     print('进入抢票页面')
@@ -23,17 +23,21 @@ if __name__ == "__main__":
     # 抢第几场的票
     group_id = 1
     d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_flowlayout"]/android.widget.FrameLayout[{group_id}]/android.widget.LinearLayout[1]').click()
-    d.sleep(0.1)
+    d.sleep(0.5)
 
     # 抢多少钱的票
     money_id = 3
     d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_price_flowlayout"]/android.widget.FrameLayout[{money_id}]/android.widget.LinearLayout[1]').click()
     d.sleep(0.2)
 
-    while not d(text="确定").exists:
-        d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_price_flowlayout"]/android.widget.FrameLayout[{1}]/android.widget.LinearLayout[1]').click()
-        d.sleep(0.1)
-        d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_price_flowlayout"]/android.widget.FrameLayout[{money_id}]/android.widget.LinearLayout[1]').click()
-        print('转换')
+    while d(text="预售").exists(timeout=5):
+        d.press("back")
+        d.sleep(0.5)
+        d.click(0.608, 0.95)
+        print('进入抢票页面')
+
+    # 需要购买张数
+    for i in range(0, 1):
+        d(resourceId="cn.damai:id/img_jia").click()
 
     d(text="确定").click()
