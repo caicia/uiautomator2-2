23f7e3be0cef955521af169578d671a4fc65c1e0
caicia <2564926167@qq.com>
2023-05-11 09:48:11 +0800

    commit

--- main.py
+++ main.py
@@ -4,11 +4,11 @@ if __name__ == "__main__":
     d = ua2.connect('xcpz4l9dfyf675kv')
     print(d.info)
     d.app_start("cn.damai")
-    d.set_fastinput_ime(False)
-    d(resourceId="cn.damai:id/homepage_advert_pb").click_exists()
+    d.click(0.923, 0.052)
     d(resourceId="cn.damai:id/channel_search_text").click()
     d.sleep(1)
-    d(resourceId="cn.damai:id/header_search_v2_input").send_keys('梁静茹世界巡回演唱会苏州站')
+    d.set_fastinput_ime(False)
+    d(resourceId="cn.damai:id/header_search_v2_input").send_keys('【深圳】五月天2023「好好好想见到你」深圳演唱会')
     d.click(0.913, 0.895)
 
     d.xpath('//*[@resource-id="cn.damai:id/one_arch_recyclerView"]/android.widget.FrameLayout[1]').click()
@@ -21,7 +21,7 @@ if __name__ == "__main__":
     print('进入抢票页面')
 
     # 抢第几场的票
-    group_id = 1
+    group_id = 2
     d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_flowlayout"]/android.widget.FrameLayout[{group_id}]/android.widget.LinearLayout[1]').click()
     d.sleep(0.5)
 
@@ -30,21 +30,30 @@ if __name__ == "__main__":
     d.xpath(f'//*[@resource-id="cn.damai:id/project_detail_perform_price_flowlayout"]/android.widget.FrameLayout[{money_id}]/android.widget.LinearLayout[1]').click()
     d.sleep(0.2)
 
-    while not d(text="价格明细").exists(timeout=5):
+    while not d(text="价格明细").exists(timeout=0.3):
         d.press("back")
         d.sleep(0.1)
         d.click(0.608, 0.95)
         print('进入抢票页面')
+        # 抢第几场的票
+        d.xpath(
+            f'//*[@resource-id="cn.damai:id/project_detail_perform_flowlayout"]/android.widget.FrameLayout[{group_id}]/android.widget.LinearLayout[1]').click()
+        d.sleep(0.5)
+
+        # 抢多少钱的票
+        d.xpath(
+            f'//*[@resource-id="cn.damai:id/project_detail_perform_price_flowlayout"]/android.widget.FrameLayout[{money_id}]/android.widget.LinearLayout[1]').click()
+        d.sleep(0.2)
 
     # 需要购买张数
-    for i in range(0, 1):
+    for i in range(0, 3):
         d(resourceId="cn.damai:id/img_jia").click()
 
     d(text="确定").click()
 
     if d(text="提交订单").exists(timeout=5):
         # 勾选实名
-        for i in range(1, 3):
+        for i in range(1, 4):
             d.xpath(f'//*[@resource-id="cn.damai:id/recycler_main"]/android.widget.LinearLayout[{i}]/android.widget.CheckBox[1]').click()
 
-        d(text="提交订单").click()
+        # d(text="提交订单").click()
